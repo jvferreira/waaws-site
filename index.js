92cd6201e464fb117a76704e3be3bcb962467731
Marcelo Zapaia <qzapaia@gmail.com>
2016-10-25 12:31:39 -0300

    redirect non html

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
 var port =  process.env.PORT || 7896;
+var path = require('path');
+var fs = require('fs');
+
 app.use(express.static(__dirname+'/public'));
+
+app.use(function(req, res){
+  if(fs.existsSync(__dirname + '/public' + req.url + '.html')){
+    res.redirect(301, req.url + '.html');
+  }else{
+    next();
+  }
+});
 
 app.listen(port,function(){
   console.log('runnnig on http://localhost:'+port);
-})
+});
